ec30a9ca0c0829e88955de55bc2edfab192fbc27
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-10-03 16:28:46 -0600

    Delete post function implemented, worked on first attempt unlike form post (=

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,17 +1,39 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 
 const Post = ({ props }) => {
 
-    return (
-        <div>
-            <div className="post-content">
-                <p> Author: {props.author}</p>
-                <p> Title: {props.title} </p>
-                <p> {props.text} </p>
-            </div>
+    let [ isDeleted, setIsDeleted ] = useState(false);
 
-            <a href="/"> Return Home </a>
+    // const navigate = useNavigate();
+
+    const handleDeletion = () => {
+        axios.delete(`http://localhost:3000/api/post/${props._id}`)
+        setIsDeleted(true);
+        // navigate("/");
+    }
+
+    return (
+        
+        <div>
+            {!isDeleted && 
+                <div className="post-content">
+                    <p> Author: {props.author}</p>
+                    <p> Title: {props.title} </p>
+                    <p> {props.text} </p>
+
+                    <a href="/"> Return Home </a>
+                    <button onClick={() => {handleDeletion()}}> Delete post </button>
+                </div>
+            }
+
+            {isDeleted &&
+                <div className="post-content">
+                    <p> Post has been deleted.</p>
+                    <p> Click <a href="/">Here</a> to return home.</p>
+                </div>
+            }
         </div>     
     )
 }
